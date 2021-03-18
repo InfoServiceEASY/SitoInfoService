d6d28147e21f2293bb04c44c497e4cab933de115
ER <localhost@root.com>
2021-03-18 10:00:09 +0100

    Create Ticketlist
    ...

---
+++ admin/ticketlist.php
@@ -0,0 +1,9 @@
+<?php
+/*lista 
+ $sql = "SELECT id, dataapertura,descrizione FROM ticket
+    WHERE isaperto = 1 AND fk_dipendente = ?"
+    $sth = $conn -> prepare($sql);
+    $sth -> bindparam('?', IDByUser($_SESSION['utente'])); //SELECT id from dipendente WHERE username = parametro ($_SESSION['utente'])   
+*/
+
+?>
