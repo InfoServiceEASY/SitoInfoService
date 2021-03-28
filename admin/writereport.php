4930d23780ab3d256e16242544e2e97c7cf75eb3
AleFed <alessandro.fedele@localhost.com>
2021-03-28 23:58:18 +0200

    Update writereport.php

--- admin/writereport.php
+++ admin/writereport.php
@@ -3,6 +3,13 @@ session_start();
 include_once("../dal.php");
 Session();
 $id = $_GET['Id'];
+function UserId($conn){
+  $sql = "SELECT id FROM utenza WHERE username = ?";
+  $sth = $conn->prepare($sql);
+  $sth->bind_param('s', $_SESSION['utente']);
+  $sth->execute();
+  return $sth -> get_result(); 
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,13 +40,17 @@ $id = $_GET['Id'];
 <?php include '../template/privatepage_params.php';
 include_once('../dal.php');
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+    //forse meglio sfruttare name della form con $_POST e passare insertreport parameters da là?
   }
-  function InsertReport($descrizione, $isrisolto_string, $fk_ticket){
+  function InsertReport($descrizione, $isrisolto_string, $durata, $fk_ticket){
     //fk_ticket in report. n report 1 ticket.
     $conn = DataConnect();
     $sql = "INSERT INTO report(ora,attività,isrisolto,fk_dipendente,fk_ticket,isconvalidato,commento) VALUES(?,?,?,?,?,?,?,?)";
-    //mettere ora attuale
+    $fk_utente = UserId(DataConnect());
+    $date = date('d/m/Y \a\l\l\e H:i:s');
+    $statement = $conn -> prepare($sql);
+    $statement -> bind_param('ssiiiis', $date, $descrizione,$isrisolto_string=='Sì'? 1:0,$fk_utente, $fk_ticket, 0, "");
+    $statement -> execute();
   }
   ?>
 
@@ -49,17 +60,17 @@ include_once('../dal.php');
   <div class="form-group">
     <label for="exampleFormControlInput1">Intervento</label>
     <!--<input class="form-control" type="text" >-->
-    <label style = "color:darkgrey;text-align:centre;content-align:centre">Intervento n. <?php echo "$id"; ?></label> <!--Nome Intervento Da DB-->
+    <label id="InterventoId" style = "color:darkgrey;text-align:centre;content-align:centre">Intervento n. <?php echo "$id"; ?></label> <!--Nome Intervento Da DB-->
   </div>
   <div class="form-group">
     <label for="exampleFormControlSelect1">Tempo impiegato (ore)?</label>
     </br>
-    <input type="text"></input>
+    <input type="text" id="OreId"></input>
     </div>
   
   <div class="form-group">
-    <label for="exampleFormControlSelect1">Problema risolto?</label>
-    <select class="form-control" id="exampleFormControlSelect1">
+    <label for="IsRisoltoId">Problema risolto?</label>
+    <select id="IsRisoltoId" class="form-control">
       <option>--</option>
       <option>S&igrave;</option>
       <option>No</option>
@@ -67,11 +78,11 @@ include_once('../dal.php');
   </div>
   <div class="form-group">
       <label for="exampleFormControlTextarea1">Descrizione</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <textarea class="form-control" id="DescrizioneId" rows="3"></textarea>
     </div>
     <div style="float: right;">
       <button class="btn btn-primary" type="submit">Cancella</button>
-      <button id="btnShowModal" type="button" class="btn btn-primary" type="submit">Conferma</button>
+      <button id="btnShowModal" type="button" class="btn btn-primary" type="submit" onclick=>Conferma</button>
 </form>
         </div>
     </div>
@@ -90,6 +101,23 @@ include_once('../dal.php');
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
     });
+    function SendJson(){
+      let id = <?php echo $id; ?>;
+      let vars = ["Ore", "IsRisolto", "Descrizione","Intervento"].map((elemento) =>{
+        return document.getElementById(elemento + "Id");
+      });
+      let data = [{
+        "Ore": vars[0].innerHTML,
+        "IsRisolto":vars[1].Childnodes[vars[1].selectedIndex].innerHTML,
+        "Descrizione":vars[2].innerHTML,
+        "FK_Ticket": vars[3].innerHTML.split('.')[1].trim()
+      }];
+      alert(data);
+      <?php $data = "<script>document.writeln(data);</script>";?>
+      <?php
+        InsertReport($data['Descrizione'], $data['IsRisolto'], $data['Ore'], $data['FK_Ticket']);
+      ?>
+    }
   </script>
 
 
