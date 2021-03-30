b75f5bee5461948f668d70a3eb46d8b24c95c56f
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2021-03-31 00:13:49 +0200

    Fixed templating and public page

---
+++ template/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-light clean-navbar">
+    <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><img src="../assets/img/logo.png" style="height: 60px;">
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item item"><a class="nav-link" href="../index.php">HOME</a></li>
+                <li class="nav-item item"><a class="nav-link" href="about-us.php">ABOUT US</a></li>
+                <li class="nav-item item"><a class="nav-link" href="contact-us.php">CONTACT US</a></li>
+                <li class="nav-item item"><a class="nav-link" href="enrollment.php">SIGN UP</a></li>
+                <li class="nav-item item"><a class="nav-link" href="login.php">LOGIN</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
