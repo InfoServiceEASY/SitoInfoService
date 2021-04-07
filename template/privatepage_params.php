d9fc98edff30aa2cc9ace2153ab17d4fa761264d
marouan ouadi <ouadimarouan@gmail.com>
2021-04-07 09:23:20 +0200

    aggiunta pagina ticket aperti

--- template/privatepage_params.php
+++ template/privatepage_params.php
@@ -18,7 +18,7 @@ switch($nome){ //dovrà essere invece $_SESSION['tipo_utente'] al posto di $nome
         break;
 }*/
 if ($_SESSION["member"] == "cliente")  {$sidebar_text = array("Dashboard", "Solutions","Ticket","Events","Status");}
-else $_SESSION["member"] == "helpdesk"? $sidebar_text = array("Dashboard","Events","Profile","Status"): $sidebar_text = array("Dashboard","TicketList","Events","Profile","Status");
+else $_SESSION["member"] == "helpdesk"? $sidebar_text = array("Dashboard","Events","Profile","Status","TicketAperti"): $sidebar_text = array("Dashboard","TicketList","Events","Profile","Status");
 ?>
 <!DOCTYPE html>
 <html lang="en">
