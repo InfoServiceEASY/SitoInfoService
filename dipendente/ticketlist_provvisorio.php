c4a501b208e03faec350121436e4d82b563e0c48
unknown <M_INF_5H@ISII.PC>
2021-04-26 10:34:55 +0200

    ticketlist simplified
    Code  Simplified :ticketlist_provvisorio.php

--- dipendente/ticketlist_provvisorio.php
+++ dipendente/ticketlist_provvisorio.php
@@ -3,119 +3,31 @@ session_start();
 include('../dal.php');
 $title = 'Interventi dipendente'; // mettere titolo più corto
 include_once '../dal.php';
-include '../template/privatepage_params.php'; ?>
-<?php
+include '../template/privatepage_params.php'; 
+
 $aperto = isset($_GET["aperto"]) ? ($_GET["aperto"] == 1 ? true : false) : null;
 $pageno = isset($_GET['pageno']) ? $_GET['pageno'] : 1;
 $conn = DataConnect();
-#region commenti
-/*$query1 = "SELECT t.id, t.dataapertura,t.descrizione , r.attività FROM ticket t INNER JOIN report r ON t.id = r.fk_ticket
- WHERE t.isaperto = 1 AND r.fk_dipendente =? and r.isconvalidato is null "; //r.attività is null, ma se si sbaglia?
-$query2 = "SELECT t.id, t.dataapertura,t.descrizione FROM ticket t INNER JOIN report r ON t.id = r.fk_ticket
- AND r.fk_dipendente =? and  t.isaperto = 0 ";
-function prova($sql, $h1, $chiuso)
-{
-  $conn = DataConnect();
-  $id = GetUser()[0];
-  $sth = $conn->prepare($sql);
-  $sth->bind_param('i', $id);
-  $sth->execute();
-  $data = $sth->get_result();
 
-  $template = "<h1 class='mt-4'>" . $h1 . "</h1>";
-  if ($data->num_rows > 0) {
-    for ($i = 0; $i < $data->num_rows; $i++) {
-      $row = $data->fetch_assoc();
-      $href = "writereport.php?Id=" . $row['id'];
-      $href2 = "measures.php?Id=" . $row['id'];
-      if ($i % 3 == 0) {
-        $template .= "<div class='containerone'>";
-      }
-      $template .= "        
-    <div class='contenitore'>
-    <p>Intervento n." . $row['id'] . " aperto il " . $row['dataapertura'] . "</p>
-    <p> " . $row['descrizione'] . "</p>
-    </br>
-    <a href='$href2'> Visualizza report sull'attività</a>
-    </br>";
-    $template .= (is_null($row['attività']))? "<a href='$href'> Scrivi report sull'attività</a> </div>" :
-    "</div>";
-      if ($i % 3 == 2) $template .= " </div>";
-    }
-    if ($data->num_rows % 3 != 0) $template .= " </div>";
-  } else
-    $template .= "<p>Al momento non hai ticket assegnati</p>";
-  $conn->close();
-  echo $template;
-}*/
-
-//prova($query1, "Interventi aperti", false);
-//prova($query2, "Interventi chiusi", true);
-#endregion 
 if (!is_null($aperto)) {
-    $nome_colonne = array("Id", "Data Apertura", "Nome", "Oggetto", "modifica");
-    $total_pages = Table($conn, $nome_colonne, $pageno, $aperto);
-    echo '<div  class="contiene">';
-    //echo "<p class='inlineLeft'  >pagina " . strval($pageno) . " su " . strval($total_pages) . " pagine</p>";
-    echo '<p style = "font-size: 200%; text-align:center;">';
-    $num_aperto = strval($aperto ? 1 : 0);
-    for ($i = 1; $i <= $total_pages; $i++)
-        echo "<a href = '?pageno=$i&aperto=$num_aperto'> $i </a>";
-    echo '</p>';
-    echo '<p style = "font-size: 200%; text-align:center;">';
-    echo "<a href='?pageno=1&aperto=$num_aperto'>   &lt;&lt;   </a>";
-?>
-    <a href="<?php if ($pageno <= 1) {
-                    echo '#';
-                } else {
-                    echo "?pageno=" . ($pageno - 1) . "&aperto=" . $num_aperto;
-                } ?>"> &lt; </a>
-    <a> <?php echo $pageno; ?> </a>
-    <a href="<?php if ($pageno >= $total_pages) {
-                    echo '#';
-                } else {
-                    echo "?pageno=" . ($pageno + 1) . "&aperto=" . $num_aperto;
-                } ?>"> &gt; </a>
-    <a href="?pageno=<?php echo $total_pages . "&aperto=" . $num_aperto; ?>"> &gt;&gt; </a>
-    </p>
-    </div>
-<?php } else { ?>
+    Table($conn, $pageno, $aperto);
+}else { ?>
     <h1 style="font-size: 300%"> Visualizza Interventi</h1>
     </br>
     </br>
     <a href="?pageno=1&aperto=0" style="text-decoration: none; color:white;">
-        <button class="btn-primary" style="float:center; width: 100%; height:100%; font-size: 200%">
-            </br>
-            </br>
-            </br>
+        <button class="btn-primary" style="float:center; width: 100%; height:100%; font-size: 200%; padding:10px; padding-bottom:100px;padding-top: 100px;">
             Visualizza i tuoi interventi chiusi
-            </br>
-            </br>
-            </br>
         </button>
     </a>
-    </br>
     </br>
     </br>
     </br>
     <a href="?pageno=1&aperto=1" style="text-decoration: none; color:white;">
-        <button class="btn-primary" style="float:center; width: 100%; height:100%; font-size: 200%">
-            </br>
-            </br>
-            </br>
+        <button class="btn-primary" style="float:center; width: 100%; height:100%; font-size: 200%; padding:10px; padding-bottom: 100px;padding-top: 100px;">
             Visualizza i tuoi interventi aperti
-            </br>
-            </br>
-            </br>
         </button>
     </a>
-    </br>
-    </br>
-    </br>
-    </br>
-    </br>
-    </br>
-    </br>
 <?php
 }
 ?>
@@ -134,16 +46,18 @@ function HeadRow($nome_colonne)
     $template .= '</tr>';
     return $template;
 }
-function TableBeginningPrint($nome_colonne)
+
+function Table_content($conn, $pageno, $total_pages, $query, $aperto)
 {
-    echo '<table id="search" style="width: 100%">';
-    echo "<thead>";
-    echo HeadRow($nome_colonne);
-    echo "</thead>";
-    echo '<tbody>';
-}
-function Table_content($conn, $query, $aperto)
-{
+    echo '<table id="search" style="width: 100%">
+    <thead>
+    <th><strong> Id </strong></td>
+    <th><strong> DataApertura </strong></td>
+    <th><strong> Nome </strong></td>
+    <th><strong> Oggetto </strong></td>
+    <th><strong> modifica</td>
+    </thead>
+    <tbody>';
     $id = GetUser()[0];
     $sth = $conn->prepare($query);
     $sth->bind_param('i', $id);
@@ -151,7 +65,7 @@ function Table_content($conn, $query, $aperto)
     $resultE = $sth->get_result();
     if ($resultE->num_rows > 0) {
         while ($row = $resultE->fetch_assoc()) {
-?>
+        ?>
             <tr>
                 <td><?php echo $row["id"]; ?></td>
                 <td><?php echo $row["DataApertura"]; ?></td>
@@ -169,18 +83,45 @@ function Table_content($conn, $query, $aperto)
         ?>
         <tr>
             <td colspan="5">No results found.</td>
-    <?php
-    }
+        </tr>
+        <?php  }
+     echo '</tbody>
+     <tfoot>
+     <th><strong> Id </strong></td>
+     <th><strong> DataApertura </strong></td>
+     <th><strong> Nome </strong></td>
+     <th><strong> Oggetto </strong></td>
+     <th><strong> modifica </strong></td>
+     </tfoot>
+     </table>';
+     echo '<div  class="contiene">';
+     //echo "<p class='inlineLeft'  >pagina " . strval($pageno) . " su " . strval($total_pages) . " pagine</p>";
+     echo '<p style = "font-size: 200%; text-align:center;">';
+     $num_aperto = strval($aperto ? 1 : 0);
+     for ($i = 1; $i <= $total_pages; $i++)
+         echo "<a href = '?pageno=$i&aperto=$num_aperto'> $i </a>";
+     echo '</p>';
+     echo '<p style = "font-size: 200%; text-align:center;">';
+     echo "<a href='?pageno=1&aperto=$num_aperto'>   &lt;&lt;   </a>";
+    ?>
+     <a href="<?php if ($pageno <= 1) {
+                     echo '#';
+                 } else {
+                     echo "?pageno=" . ($pageno - 1) . "&aperto=" . $num_aperto;
+                 } ?>"> &lt; </a>
+     <a> <?php echo $pageno; ?> </a>
+     <a href="<?php if ($pageno >= $total_pages) {
+                     echo '#';
+                 } else {
+                     echo "?pageno=" . ($pageno + 1) . "&aperto=" . $num_aperto;
+                 } ?>"> &gt; </a>
+     <a href="?pageno=<?php echo $total_pages . "&aperto=" . $num_aperto; ?>"> &gt;&gt; </a>
+     </p>
+    </div>
+<?php
 }
-function TableEndPrint($nome_colonne)
-{
-    echo "</tbody>
-        <tfoot>";
-    echo HeadRow($nome_colonne);
-    echo "</tfoot>
-        </table>";
-}
-function Table($conn, $nome_colonne, $pageno, $aperto)
+
+function Table($conn, $pageno, $aperto)
 {
     $no_of_records_per_page = 10;
     $offset = ($pageno - 1) * $no_of_records_per_page;
@@ -194,33 +135,21 @@ function Table($conn, $nome_colonne, $pageno, $aperto)
     $sth->bind_param('i', $id);
     $sth->execute();
     $result = $sth->get_result();
-    //$result = mysqli_query($conn, $total_pages_sql);
     $total_rows = mysqli_fetch_array($result)["TotalRows"];
     $total_pages = floor($total_rows / $no_of_records_per_page) + 1;
-    $query = null;
-    if ($aperto) {
-        $query = "SELECT t.id, t.dataapertura AS DataApertura,t.descrizione AS Oggetto, r.attività AS Nome FROM ticket t INNER JOIN report r ON t.id = r.fk_ticket
-        WHERE t.isaperto = 1 AND r.fk_dipendente =? and r.isconvalidato is null LIMIT $offset, $no_of_records_per_page"; //r.attività is null, ma se si sbaglia?
-    } else {
-        $query = "SELECT t.id, t.dataapertura AS DataApertura,t.descrizione AS Oggetto, r.attività AS Nome FROM ticket t INNER JOIN report r ON t.id = r.fk_ticket
-            AND r.fk_dipendente =? and  t.isaperto = 0 LIMIT $offset, $no_of_records_per_page";
-    }
+    $query = $aperto?   
+    "SELECT t.id, t.dataapertura AS DataApertura,t.descrizione AS Oggetto, r.attività AS Nome FROM ticket t INNER JOIN report r ON t.id = r.fk_ticket
+    WHERE t.isaperto = 1 AND r.fk_dipendente =? and r.isconvalidato is null LIMIT $offset, $no_of_records_per_page"
+    : 
+    "SELECT t.id, t.dataapertura AS DataApertura,t.descrizione AS Oggetto, r.attività AS Nome FROM ticket t INNER JOIN report r ON t.id = r.fk_ticket
+        AND r.fk_dipendente =? and  t.isaperto = 0 LIMIT $offset, $no_of_records_per_page";
     //si comincia a stampare
-    //aperti (o chiusi)
-    TableBeginningPrint($nome_colonne);
-    Table_content($conn, $query, $aperto);
-    TableEndPrint($nome_colonne);
-    //chiusi (vecchia versione)
-    /*echo "</br>";
-    TableBeginningPrint($nome_colonne);
-    Table_content($conn, $query2, false);
-    TableEndPrint($nome_colonne);*/
-    return $total_pages;
+    //aperti o chiusi
+    Table_content($conn, $pageno, $total_pages, $query, $aperto);
 }
 #endregion Methods
-    ?>
-    <br>
-    </div>
-    </body>
-
-    </html>
+?>
+<br>
+</div>
+</body>
+</html>
