3ef3816c270edda43c5a137af9c96fc687f35c99
AleFed <alessandro.fedele@localhost.com>
2021-05-14 17:58:18 +0200

    table changes
    partial table bug fixing

--- dipendente/interventi.php
+++ dipendente/interventi.php
@@ -98,33 +98,13 @@ if (ReportOfthis($_GET['id'])) {
         window.location.href='Ticketlist.php';
         </script>");
         }
-        echo '<div  class="contiene">
-            <p style = "font-size: 200%; text-align:center;">';
-        $num_aperto = strval($aperto ? 1 : 0);
-        for ($i = 1; $i <= $total_pages; $i++)
-            echo "<a href = '?pageno=$i&aperto=$num_aperto'> $i </a>";
-        echo '</p>';
-        echo '<p style = "font-size: 200%; text-align:center;">';
-        echo "<a href='?pageno=1&aperto=$num_aperto'>   &lt;&lt;   </a>";
+        echo '<div  class="contiene">';
+        Paginazione($pageno, $total_pages);
         ?>
-        <a href="<?php if ($pageno <= 1) {
-                        echo '#';
-                    } else {
-                        echo "?pageno=" . ($pageno - 1) . "&aperto=" . $num_aperto;
-                    } ?>"> &lt; </a>
-        <a> <?php echo $pageno; ?> </a>
-        <a href="<?php if ($pageno >= $total_pages) {
-                        echo '#';
-                    } else {
-                        echo "?pageno=" . ($pageno + 1) . "&aperto=" . $num_aperto;
-                    } ?>"> &gt; </a>
-        <a href="?pageno=<?php echo $total_pages . "&aperto=" . $num_aperto; ?>"> &gt;&gt; </a>
-        </p>
         </div>
         <br>
         </div>
         </body>
-
         </html>
 <?php
     } else {
